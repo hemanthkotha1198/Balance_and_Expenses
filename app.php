75608542563f6b4aaf37ba1703e0f486364ebe7d
prannethkotha-1198 <123263232+prannethkotha-1198@users.noreply.github.com>
2023-07-16 22:19:06 +0530

    Add files via upload

---
+++ app.php
@@ -0,0 +1,60 @@
+<?php
+declare(strict_types=1);
+function getTranctionFiles(string $dirpath):array
+{
+    $files=[];
+    foreach (scandir($dirpath) as $file) {
+        IF(is_dir($file)){
+            continue;
+        }
+        $files[]=$dirpath.$file;
+    }
+    return $files;
+}
+function getTransactions(string $fileName,? callable $transactionHandler= null):array
+{
+    if (!file_exists($fileName)) {
+        trigger_error('File"'.$fileName.'"does not exists.',E_USER_ERROR);
+    }
+    $file=fopen($fileName,'r');
+    fgetcsv($file);
+    $transaction=[];
+    while (($transaction=fgetcsv($file))!== false) {
+        # code...
+        if ($transactionHandler!==null) {
+            $transaction=$transactionHandler($transaction);
+        }
+        $transactions[]=$transaction;
+    }
+    return $transactions;
+}
+function  extractTransaction(array $transactionRow):array
+{
+    [$date,$checkNumber,$description,$amount]=$transactionRow;
+    $amount=(float)str_replace(['$',','],'',$amount);
+    return[
+        'date'=>$date,
+        'checkNumber'=>$checkNumber,
+        'description'=>$description,
+        'amount'=>$amount,
+    ];
+}
+function calculateTotals(array $transactions):array
+{
+    $totals=['netTotal'=>0,'totalIncome'=>0,'totalExpense'=>0];
+    foreach ($transactions as $transaction) {
+    $totals['netTotal']+=$transaction['amount'];
+    if($transaction['amount']>=0)
+    {
+        $totals['totalIncome']+=$transaction['amount'];
+    }
+    else{   
+     $totals['totalExpense']+=$transaction['amount'];
+    }
+    }
+
+return $totals;
+
+
+
+}
